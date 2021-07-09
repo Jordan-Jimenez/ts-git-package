5c5b3f2e2c37d02721b6dd857b322aa9ba8e96c0
Jordan-Jimenez <jordan.jimenez.contact@gmail.com>
2021-07-09 15:43:28 -0700

    added dist directory

---
+++ dist/component/text/index.d.ts
@@ -0,0 +1,6 @@
+import { FC } from "react";
+interface ITextProps {
+    color?: string;
+}
+declare const Text: FC<ITextProps>;
+export default Text;
---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+import Text from "./component/text";
+export { Text };
